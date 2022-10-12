6e53b9a877b7866a3672347f1d31a3e283c81da7
Aflc0 <victorodrigues.alves@outlook.com>
2022-10-11 21:48:40 -0300

    Atualizada index

--- public/js/main.js
+++ public/js/main.js
@@ -8,53 +8,54 @@ function toggleMenu() {
 
 btnDropdown.addEventListener("click", toggleMenu);
 
-//Carrossel de cursos
-var mySwiper = new Swiper(".swiper-container", {
-  // Optional parameters
-  spaceBetween: 15,
-  slidesPerView: 2,
-  loop: true,
-  freeMode: true,
-  loopAdditionalSlides: 5,
-  speed: 500,
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 3,
-      slidesPerGroup: 5,
-      freeMode: false,
-    },
-    920: {
-      slidesPerView: 4,
-      slidesPerGroup: 5,
-      freeMode: false,
-    },
-    1024: {
-      slidesPerView: 5,
-      slidesPerGroup: 5,
-      freeMode: false,
-    },
-  },
-});
-
-//TypeWritting Effect
-var tickr = [
-  '<del style="color: white" ><ins>Connecting...</ins><ins>1.5</ins></del>',
-  '<span style="color:white;">Seja bem vindo ao<br></span><span style="color: blue">Code Guide<ins>3</ins></span>',
-];
-
-$("#demo_3").t(tickr.join((x = "<ins>1.2</ins><del>*</del>")) + x, {
-  speed: 45,
-  repeat: true,
-  pause_on_click: true,
-});
-
+//Jquery Starts
 $(document).ready(function () {
+  //Carrossel de cursos
+  var mySwiper = new Swiper(".swiper-container", {
+    // Optional parameters
+    spaceBetween: 15,
+    slidesPerView: 2,
+    loop: true,
+    freeMode: true,
+    loopAdditionalSlides: 5,
+    speed: 500,
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      // when window width is >= 640px
+      570: {
+        slidesPerView: 3,
+        slidesPerGroup: 5,
+        freeMode: false,
+      },
+      920: {
+        slidesPerView: 4,
+        slidesPerGroup: 5,
+        freeMode: false,
+      },
+      1024: {
+        slidesPerView: 5,
+        slidesPerGroup: 5,
+        freeMode: false,
+      },
+    },
+  });
+
+  //TypeWritting Effect
+  var tickr = [
+    '<del style="color: white" ><ins>Connecting...</ins><ins>1.5</ins></del>',
+    '<span style="color:white;">Seja bem vindo ao<br></span><span style="color: blue">Code Guide<ins>3</ins></span>',
+  ];
+
+  $("#demo_3").t(tickr.join((x = "<ins>1.2</ins><del>*</del>")) + x, {
+    speed: 45,
+    repeat: true,
+    pause_on_click: true,
+  });
+
   $(".highlight").css("display", "none");
 
   $(".thumbTile").click(function () {
@@ -70,6 +71,10 @@ $(document).ready(function () {
       1000
     );
   });
+});
+
+$(".close-highlight-button").click(function () {
+  $(".highlight").slideToggle("slow");
 });
 
 //Menu Mobile
