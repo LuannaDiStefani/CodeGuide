f10b12c7bad0ec592efdddad3af2e2a486102f6a
Alberto Junior <albertojunior.05@gmail.com>
2022-10-18 18:15:39 -0300

    Página cursos

--- public/js/main.js
+++ public/js/main.js
@@ -10,155 +10,159 @@ btnDropdown.addEventListener("click", toggleMenu);
 
 //Jquery Starts
 $(document).ready(function () {
-  $(".highlight").css("display", "none");
+  //Menu Mobile
+  const myMedia = window.matchMedia("(max-width: 595px)");
+  let searchButton = document.querySelectorAll(".mobile-search-button");
+  const searchBox = document.querySelector(".search-bar input");
+  const categoryButton = document.querySelector(".category-button");
 
-  $(".thumbTile").click(function () {
-    event.preventDefault();
-    if ($(".highlight").css("display") == "none") {
-      $(".highlight").slideToggle("slow");
+  function responsive(media) {
+    if (media.matches) {
+      $(".search-bar i").addClass("mobile-search-button");
+      searchButton = document.querySelectorAll(".mobile-search-button");
+      $(searchBox).removeClass("search-active").css("width", "100%");
+      $(searchBox).addClass("search-inactive").css("width", "20%");
+      searchButton.forEach((item) => {
+        $(item).on("click", function () {
+          event.preventDefault();
+          if ($(searchBox).css("visibility") == "hidden") {
+            $(searchBox).removeClass("search-inactive");
+            $(searchBox).addClass("search-active").css("width", "100%");
+          } else {
+            $(searchBox).removeClass("search-active");
+            $(searchBox).addClass("search-inactive").css("width", "20%");
+          }
+        });
+      });
+    } else {
+      $(".search-bar i").removeClass("mobile-search-button");
+      $(searchBox).removeClass("search-inactive");
+      $(searchBox).addClass("search-active").css("width", "100%");
     }
-    $(".highlight").css("display", "block");
-    $("html, body").animate(
-      {
-        scrollTop: $(".highlight").offset().top - 60,
-      },
-      1000
-    );
+    //Página cadastro - Menu
+    if (media.matches) {
+      $(".admin-menu ul li").hover(
+        function () {
+          $(this).append(
+            $(
+              "<h4>" +
+                $(this).children("a").children("i").attr("title") +
+                "</h4>"
+            )
+          );
+        },
+        function () {
+          $(this).find("h4").last().remove();
+        }
+      );
+    } else {
+      $(".admin-menu ul li").hover(function () {
+        $(this).find("h4").last().remove();
+      });
+    }
+  }
+
+  responsive(myMedia);
+  myMedia.addListener(responsive);
+
+  //Menu Lateral
+  $(categoryButton).click(function () {
+    event.preventDefault();
+    if ($(".mobile-category").css("visibility") == "hidden") {
+      $(".mobile-category")
+        .css("visibility", "visible")
+        .css("opacity", "1")
+        .css("width", "200px");
+    } else {
+      $(".mobile-category")
+        .css("visibility", "hidden")
+        .css("opacity", "0")
+        .css("width", "0");
+    }
+  });
+
+  //Página de Perfil - Modal
+  $(".open-model").click(function () {
+    $(".model-container").css("transform", "scale(1)");
+    setTimeout(function () {
+      $(".model-container").css("background", "rgba(0, 0, 0, 0.8)");
+    }, 400);
+  });
+
+  $(".course-comment-icon").click(function () {
+    $(".model-container").css("transform", "scale(1)");
+    setTimeout(function () {
+      $(".model-container").css("background", "rgba(0, 0, 0, 0.8)");
+    }, 400);
+  });
+
+  $(".close-button").click(function () {
+    $(".model-container").css("background", "transparent");
+    setTimeout(function () {
+      $(".model-container").css("transform", "scale(0)");
+    }, 200);
+  });
+
+  //Editar Imagem Efeito
+  $(".edit-user-picture").hover(
+    function () {
+      $(".edit-user-picture img").css("opacity", "0.3");
+      $(".edit-user-picture span").fadeTo(100, 0.15, function () {
+        $(this).css("opacity", "1");
+      });
+    },
+    function () {
+      $(".edit-user-picture img").css("opacity", "1");
+      $(".edit-user-picture span").fadeTo(100, 0.15, function () {
+        $(this).css("opacity", "0");
+      });
+    }
+  );
+
+  //Upload Button
+  let uploadButton = document.getElementById("upload-button");
+  let choosenImg = document.getElementById("choosen-img");
+  let fileName = document.getElementById("file-name");
+
+  $(uploadButton).change(function () {
+    let reader = new FileReader();
+    reader.readAsDataURL(uploadButton.files[0]);
+    console.log(uploadButton.files[0]);
+    reader.onload = () => {
+      choosenImg.setAttribute("src", reader.result);
+    };
+    fileName.textContent = uploadButton.files[0].name;
+  });
+
+  //Target - Edit-field
+  let targetDivs = document.querySelectorAll(".edit-field");
+  $(targetDivs).hide();
+  $(targetDivs[0]).show();
+  $(".target-edit-field").click(function () {
+    event.preventDefault();
+    $(".edit-field").hide();
+    $(".target" + $(this).attr("target")).show();
   });
 });
 
-$(".close-highlight-button").click(function () {
-  $(".highlight").slideToggle("slow");
-});
+//Highlight Target
+let highlights = document.querySelectorAll(".highlight");
+$(highlights).hide();
 
-//Menu Mobile
-const myMedia = window.matchMedia("(max-width: 595px)");
-let searchButton = document.querySelectorAll(".mobile-search-button");
-const searchBox = document.querySelector(".search-bar input");
-const categoryButton = document.querySelector(".category-button");
-
-function responsive(media) {
-  if (media.matches) {
-    $(".search-bar i").addClass("mobile-search-button");
-    searchButton = document.querySelectorAll(".mobile-search-button");
-    $(searchBox).removeClass("search-active").css("width", "100%");
-    $(searchBox).addClass("search-inactive").css("width", "20%");
-    searchButton.forEach((item) => {
-      $(item).on("click", function () {
-        event.preventDefault();
-        if ($(searchBox).css("visibility") == "hidden") {
-          $(searchBox).removeClass("search-inactive");
-          $(searchBox).addClass("search-active").css("width", "100%");
-        } else {
-          $(searchBox).removeClass("search-active");
-          $(searchBox).addClass("search-inactive").css("width", "20%");
-        }
-      });
-    });
-  } else {
-    $(".search-bar i").removeClass("mobile-search-button");
-    $(searchBox).removeClass("search-inactive");
-    $(searchBox).addClass("search-active").css("width", "100%");
-  }
-  //Página cadastro - Menu
-  if (media.matches) {
-    $(".admin-menu ul li").hover(
-      function () {
-        $(this).append(
-          $(
-            "<h4>" + $(this).children("a").children("i").attr("title") + "</h4>"
-          )
-        );
-      },
-      function () {
-        $(this).find("h4").last().remove();
-      }
-    );
-  } else {
-    $(".admin-menu ul li").hover(function () {
-      $(this).find("h4").last().remove();
-    });
-  }
-}
-
-responsive(myMedia);
-myMedia.addListener(responsive);
-
-//Menu Lateral
-$(categoryButton).click(function () {
+$(".thumbTile").click(function () {
   event.preventDefault();
-  if ($(".mobile-category").css("visibility") == "hidden") {
-    $(".mobile-category")
-      .css("visibility", "visible")
-      .css("opacity", "1")
-      .css("width", "200px");
-  } else {
-    $(".mobile-category")
-      .css("visibility", "hidden")
-      .css("opacity", "0")
-      .css("width", "0");
-  }
-});
-
-//Página de Perfil - Modal
-$(".open-model").click(function () {
-  $(".model-container").css("transform", "scale(1)");
-  setTimeout(function () {
-    $(".model-container").css("background", "rgba(0, 0, 0, 0.8)");
-  }, 400);
-});
-
-$(".course-comment-icon").click(function () {
-  $(".model-container").css("transform", "scale(1)");
-  setTimeout(function () {
-    $(".model-container").css("background", "rgba(0, 0, 0, 0.8)");
-  }, 400);
-});
-
-$(".close-button").click(function () {
-  $(".model-container").css("background", "transparent");
-  setTimeout(function () {
-    $(".model-container").css("transform", "scale(0)");
-  }, 200);
-});
-
-//Editar Imagem Efeito
-$(".edit-user-picture").hover(
-  function () {
-    $(".edit-user-picture img").css("opacity", "0.3");
-    $(".edit-user-picture span").fadeTo(100, 0.15, function () {
-      $(this).css("opacity", "1");
-    });
-  },
-  function () {
-    $(".edit-user-picture img").css("opacity", "1");
-    $(".edit-user-picture span").fadeTo(100, 0.15, function () {
-      $(this).css("opacity", "0");
-    });
-  }
-);
-
-//Upload Button
-let uploadButton = document.getElementById("upload-button");
-let choosenImg = document.getElementById("choosen-img");
-let fileName = document.getElementById("file-name");
-
-$(uploadButton).change(function () {
-  let reader = new FileReader();
-  reader.readAsDataURL(uploadButton.files[0]);
-  console.log(uploadButton.files[0]);
-  reader.onload = () => {
-    choosenImg.setAttribute("src", reader.result);
-  };
-  fileName.textContent = uploadButton.files[0].name;
-});
-
-//Target - Edit-field
-let targetDivs = document.querySelectorAll(".edit-field");
-$(targetDivs).hide();
-$(targetDivs[0]).show();
-$(".target-edit-field").click(function () {
-  event.preventDefault();
-  $(".edit-field").hide();
-  $(".target" + $(this).attr("target")).show();
+  $(highlights).hide();
+  let target = $(this).parent().parent().parent().parent().attr("target");
+  let thisHighlight = highlights[target];
+  console.log(thisHighlight);
+  $(thisHighlight).slideToggle("slow");
+  $("html, body").animate(
+    {
+      scrollTop: $(thisHighlight).offset().top - 70,
+    },
+    1000
+  );
+  $(".close-highlight-button").click(function () {
+    $(thisHighlight).hide("slow");
+  });
 });
